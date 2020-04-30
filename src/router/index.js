6b85c70e190989aca4d17036235c1dca1183572a
dongxiuling <644961699@qq.com>
2020-04-30 11:33:21 +0800

    admin

---
+++ src/router/index.js
@@ -0,0 +1,149 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+/**
+ * Note: sub-menu only appear when route children.length >= 1
+ * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * hidden: true                   if set true, item will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar
+    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+  }
+ */
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: '首页', icon: 'dashboard' },
+    }]
+  },
+
+  // {
+  //   path: '/example',
+  //   component: Layout,
+  //   redirect: '/example/table',
+  //   name: 'Example',
+  //   meta: { title: 'Example', icon: 'example' },
+  //   children: [
+  //     {
+  //       path: 'table',
+  //       name: 'Table',
+  //       component: () => import('@/views/table/index'),
+  //       meta: { title: 'Table', icon: 'table' }
+  //     },
+  //     {
+  //       path: 'tree',
+  //       name: 'Tree',
+  //       component: () => import('@/views/tree/index'),
+  //       meta: { title: 'Tree', icon: 'tree' }
+  //     }
+  //   ]
+  // },
+
+  {
+    path: '/company',
+    component: Layout,
+    meta: { title: '公司概况', icon: 'form' },
+    children: [
+      {
+        path: 'index',
+        name: 'company',
+        component: () => import('@/views/company/index'),
+        meta: { title: '公司简介', icon: 'form' }
+      },
+      {
+        path: 'qualification',
+        name: 'qualification',
+        component: () => import('@/views/company/qualification'),
+        meta: { title: '公司资质', icon: 'table' }
+      },
+      {
+        path: 'ceo',
+        name: 'Tree',
+        component: () => import('@/views/company/ceo'),
+        meta: { title: '董事长致辞', icon: 'ceo' }
+      },
+      {
+        path: 'organization',
+        name: 'organization',
+        component: () => import('@/views/company/organization'),
+        meta: { title: '组织结构', icon: 'tree' }
+      },
+      {
+        path: 'honor',
+        name: 'honor',
+        component: () => import('@/views/company/honor'),
+        meta: { title: '公司荣誉', icon: 'example' }
+      },
+      {
+        path: 'branch',
+        name: 'branch',
+        component: () => import('@/views/company/branch'),
+        meta: { title: '分支机构', icon: 'list' }
+      },
+      {
+        path: 'add',
+        name: 'add',
+        component: () => import('@/views/company/addQua'),
+        meta: { title: '添加资质', icon: 'list' },
+        hidden:true
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
---
+++ src/views/company/qualification.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="app-container">
+    <el-button type="primary" style="margin-bottom:20px" size="mini" @click="$router.push('/company/add')">添加资质</el-button>
+    <el-table
+      style="width: 100%"
+      border
+      :data="tableData.filter(data => !search || data.title.toLowerCase().includes(search.toLowerCase()))"
+    >
+      <el-table-column label="公司资质" prop="title"></el-table-column>
+      <el-table-column label="时间" prop="date"></el-table-column>
+      <el-table-column align="right">
+        <template slot="header">
+          <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
+        </template>
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          title: "王小虎"
+        },
+        {
+          date: "2016-05-04",
+          title: "王小虎"
+        },
+        {
+          date: "2016-05-01",
+          title: "张小虎"
+        },
+        {
+          date: "2016-05-03",
+          title: "王小虎"
+        }
+      ],
+      search: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
